ebf2224a3879a50a21ba23082c6103c6552089ac
LoveIsForSuckers <mitexxx94@rambler.ru>
2018-03-17 20:45:08 +0300

    creation - part 1

---
+++ AspNetCoreMvc/AspNetCoreSolution/Startup.cs
@@ -0,0 +1,52 @@
+﻿using AspNetCoreSolution.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.MongoDB;
+
+namespace AspNetCoreSolution
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddIdentityWithMongoStores(Configuration.GetConnectionString("DefaultConnection"));
+
+            services.AddMvc();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
